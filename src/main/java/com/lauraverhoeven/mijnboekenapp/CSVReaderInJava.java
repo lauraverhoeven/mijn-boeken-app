f140f86d90c4d81878c8ea813d0a49409a789981
lauraverhoeven <laurasuzanna.verhoeven@gmail.com>
2022-11-02 22:14:47 +0100

    Added 'books.csv' as database, and files to read the csv and return a list of books

---
+++ src/main/java/com/lauraverhoeven/mijnboekenapp/CSVReaderInJava.java
@@ -0,0 +1,64 @@
+package com.lauraverhoeven.mijnboekenapp;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+
+public class CSVReaderInJava {
+
+    public static void main(String... args) {
+    }
+
+    public static List<Book> readBooksFromCSV(String fileName) {
+        List<Book> books = new ArrayList<>();
+        Path pathToFile = Paths.get(fileName);
+
+        // create an instance of BufferedReader
+        // using try with resource, Java 7 feature to close resources
+        try (BufferedReader br = Files.newBufferedReader(pathToFile,
+                StandardCharsets.US_ASCII)) {
+
+            // read the first line from the text file
+            String line = br.readLine();
+
+            // loop until all lines are read
+            while (line != null) {
+
+                // use string.split to load a string array with the values from
+                // each line of
+                // the file, using a comma as the delimiter
+                String[] attributes = line.split(",");
+
+                Book book = createBook(attributes);
+
+                // adding book into ArrayList
+                books.add(book);
+
+                // read next line before looping
+                // if end of file reached, line would be null
+                line = br.readLine();
+            }
+
+        } catch (IOException ioe) {
+            ioe.printStackTrace();
+        }
+
+        return books;
+    }
+
+    private static Book createBook(String[] metadata) {
+        String title = metadata[0];
+        String author = metadata[1];
+        String year = metadata[2];
+
+        // create and return book of this metadata
+        return new Book(title, author, year);
+    }
+}
+
+
--- src/main/java/com/lauraverhoeven/mijnboekenapp/MijnBoekenAppApplication.java
+++ src/main/java/com/lauraverhoeven/mijnboekenapp/MijnBoekenAppApplication.java
@@ -2,12 +2,18 @@ package com.lauraverhoeven.mijnboekenapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import java.util.List;
 
 @SpringBootApplication
 public class MijnBoekenAppApplication {
 
 	public static void main(String[] args) {
+		List<Book> Books = CSVReaderInJava.readBooksFromCSV("src/main/resources/static/books/books.csv");
+		System.out.println(Books.get(0));
+
 		SpringApplication.run(MijnBoekenAppApplication.class, args);
 	}
 
+
 }
+
