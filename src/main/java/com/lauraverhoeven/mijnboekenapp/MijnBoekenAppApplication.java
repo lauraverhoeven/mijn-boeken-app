2c538d747aed6a48b2c3032b76fe410a36f40c69
lauraverhoeven <laurasuzanna.verhoeven@gmail.com>
2022-11-02 20:16:34 +0100

    Initializing Spring Boot Framework for 'Mijn Boeken App'

---
+++ src/main/java/com/lauraverhoeven/mijnboekenapp/MijnBoekenAppApplication.java
@@ -0,0 +1,13 @@
+package com.lauraverhoeven.mijnboekenapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MijnBoekenAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MijnBoekenAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lauraverhoeven/mijnboekenapp/MijnBoekenAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lauraverhoeven.mijnboekenapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MijnBoekenAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
